50e7177809dac5c42a07c234aa605e6c0d91129c
lixiruijack <lixiruijack@gmail.com>
2015-12-24 16:15:31 +0800

    New project created

---
+++ MagicCalculator/MagicCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MagicCalculator
+//
+//  Created by Jack Li on 12/24/15.
+//  Copyright © 2015 Jack Li. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ MagicCalculator/MagicCalculator/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MagicCalculator
+//
+//  Created by Jack Li on 12/24/15.
+//  Copyright © 2015 Jack Li. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
